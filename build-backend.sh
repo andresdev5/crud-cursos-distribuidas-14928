f892b8adf7dd16d5e30edacfab78f7f989d54c01
Andrés Jácome <ajjacome2@espe.edu.ec>
2024-08-29 23:29:56 -0500

    Fixed permissions

--- build-backend.sh
+++ build-backend.sh
@@ -3,6 +3,8 @@
 # Guarda el directorio actual
 rootpath=$(pwd)
 
+chmod +rwx "$rootpath/build-backend.sh"
+
 # Navega a la carpeta 'backend'
 cd "$rootpath/backend"
 
@@ -12,6 +14,12 @@ gateway_service_path="$PWD/msvc-gateway"
 eureka_service_path="$PWD/msvc-registry"
 users_service_path="$PWD/msvc-users"
 courses_service_path="$PWD/msvc-courses"
+
+chmod +rwx "$auth_service_path/build.sh"
+chmod +rwx "$gateway_service_path/build.sh"
+chmod +rwx "$eureka_service_path/build.sh"
+chmod +rwx "$users_service_path/build.sh"
+chmod +rwx "$courses_service_path/build.sh"
 
 echo "Building backend services"
 
